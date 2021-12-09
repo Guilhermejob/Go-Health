2ee86ef5fb0ae5111211f6c45806fb0452493748
Miqueias <miqs_carvalho@hotmail.com>
2021-12-09 10:14:16 -0300

    feat: add route init for test

--- app/routes/client_blueprint.py
+++ app/routes/client_blueprint.py
@@ -6,3 +6,6 @@ bp_clients = Blueprint('bp_clients', __name__, url_prefix='/clients')
 
 bp_clients.post('')(create_client)
 bp_clients.get('/<int:id>')(get_client)
+@bp_clients.get("")
+def init():
+    return "funcionou!",200
