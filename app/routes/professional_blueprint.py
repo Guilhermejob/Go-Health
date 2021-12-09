98e3c80acef5c78c3b88985ac27b9caccdd2d475
Miqueias <miqs_carvalho@hotmail.com>
2021-12-09 19:51:20 -0300

    deploy v3

--- app/routes/professional_blueprint.py
+++ app/routes/professional_blueprint.py
@@ -12,4 +12,11 @@ bp_professional.get('/<int:id>')(get_by_id)
 bp_teste = Blueprint('bp_teste',__name__)
 @bp_teste.get("/")
 def testando():
-    return "está funcionando!!",200
+    return {"rotas":{
+        "get_all_clients": "/clients",
+        "get_client_by_id": "/clients/<int:id>",
+        "create_client": "/clients",
+        "get_all_professionals": "/professional",
+        "get_professional_by_id": "/professional/<int:id>",
+        "create_professional": "/professional"
+    }},200
