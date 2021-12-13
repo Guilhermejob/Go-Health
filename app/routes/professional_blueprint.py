c5f5b3d41e53a726929824861880bb0d7c9ddefb
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2021-12-13 10:55:57 -0300

    feat: finalizing food_plan route.

--- app/routes/professional_blueprint.py
+++ app/routes/professional_blueprint.py
@@ -8,15 +8,3 @@ bp_professional = Blueprint(
 bp_professional.post('')(create)
 bp_professional.get('')(get_all)
 bp_professional.get('/<int:id>')(get_by_id)
-
-bp_teste = Blueprint('bp_teste',__name__)
-@bp_teste.get("/")
-def testando():
-    return {"rotas":{
-        "get_all_clients": "/clients",
-        "get_client_by_id": "/clients/<int:id>",
-        "create_client": "/clients",
-        "get_all_professionals": "/professional",
-        "get_professional_by_id": "/professional/<int:id>",
-        "create_professional": "/professional"
-    }},200
