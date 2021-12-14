e50276f393e45eb5805b46ecdd74386a07ba447c
Miqueias <miqs_carvalho@hotmail.com>
2021-12-14 11:56:19 -0300

    feat: route delete

--- app/routes/client_blueprint.py
+++ app/routes/client_blueprint.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
-from app.controllers.client_controllers import create, get_by_id, get_all,update
+from app.controllers.client_controllers import create, get_by_id, get_all,update,delete
 from flask_jwt_extended import jwt_required
 
 bp_clients = Blueprint('bp_clients', __name__, url_prefix='/clients')
 
 
 bp_clients.post('')(create)
-bp_clients.patch('/<int:id>')(jwt_required()(update))
-bp_clients.get('/<int:id>')(jwt_required()(get_by_id))
 bp_clients.get("")(get_all)
+bp_clients.patch('/<int:id>')(jwt_required()(update))
+bp_clients.delete('/<int:id>')(jwt_required()(delete))
+bp_clients.get('/<int:id>')(jwt_required()(get_by_id))
