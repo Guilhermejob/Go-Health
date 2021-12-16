f8c927a9329ad1d5c52773fb283fd515545c31ff
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2021-12-16 11:47:42 -0300

    fix: adjusted some models and migrations

--- app/models/disease_client.py
+++ app/models/disease_client.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, ForeignKey
 diseaseclientmodel = db.Table('disease_client',
 
                               Column('client_id', Integer,
-                                     ForeignKey('clients.id')),
+                                     ForeignKey('clients.id', ondelete='CASCADE')),
                               Column('disease_id', Integer, ForeignKey(
                                      'diseases.disease_id'))
 
--- app/models/surgery_client.py
+++ app/models/surgery_client.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, ForeignKey
 surgeryclientmodel = db.Table('surgery_client',
 
                               Column('client_id', Integer,
-                                     ForeignKey('clients.id')),
+                                     ForeignKey('clients.id', ondelete='CASCADE')),
                               Column('surgery_id', Integer, ForeignKey(
                                      'surgeries.surgery_id'))
 
